5dc502ce77441142263b4e9de3d66e59b00c0dca
Freeeemon <99979047+smn-dst@users.noreply.github.com>
2026-02-05 15:17:45 +0100

    fix: health cypress/smoky

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://127.0.0.1:8000',
+    baseUrl: 'http://127.0.0.1:8000/health',
     supportFile: false,
     specPattern: 'tests/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
--- tests/e2e/smoke.cy.js
+++ tests/e2e/smoke.cy.js
@@ -1,6 +1,6 @@
 describe('Smoke E2E', () => {
-  it('la page d\'accueil répond', () => {
-    cy.visit('/home');
+  it('la page d\'health répond', () => {
+    cy.visit('/health');
     cy.url().should('include', '127.0.0.1:8000');
   });
 });
