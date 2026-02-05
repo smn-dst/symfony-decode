5bc241e99cdb2abdf599b67b1d37f8bfacfd8619
Freeeemon <99979047+smn-dst@users.noreply.github.com>
2026-02-05 15:23:02 +0100

    fix: baseUrl

--- tests/e2e/smoke.cy.js
+++ tests/e2e/smoke.cy.js
@@ -1,6 +1,6 @@
 describe('Smoke E2E', () => {
   it('la page d\'health répond', () => {
     cy.visit('/health');
-    cy.url().should('include', '127.0.0.1:8000');
+    cy.url().should('include', '/health');
   });
 });
